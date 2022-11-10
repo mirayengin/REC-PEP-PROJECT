20dbe12e5e7d7893168d1b85eda23143d6ebf3dc
erhan <erhanserkansabriye@gmail.com>
2022-11-11 02:00:52 +0300

    about eklendi

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Image } from 'react-bootstrap'
 import NavbarComp from '../../components/nav/Navbar'
+import constPhoto from "../../assets/const.png";
+import { AboutDiv, ContainertDiv } from './About.style';
 
 const About = () => {
   return (
-    <div>
+    <ContainertDiv>
       <NavbarComp />
-    </div>
+      <AboutDiv>
+        <div>
+          <Image style={{width:"25%"}} src={constPhoto} alt=""/>
+        </div>
+        <p className='mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto reprehenderit error ad porro, labore tenetur.</p>
+      </AboutDiv>
+    </ContainertDiv>
   )
 }
 
---
+++ src/pages/about/About.style.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const AboutDiv = styled.div`
+ display:flex;
+ flex-direction:column;
+ justify-content:center;
+ align-items:center;
+ margin-top:10rem;
+`;
+
+export const ContainertDiv = styled.div`
+ height:100vh;
+ background-color:lightgoldenrodyellow;
+`;
+
+
+
